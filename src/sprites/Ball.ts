f5caf2bea761a68b70218cc197abede9c5874d93
dCieszynski <dCieszynski99@gmail.com>
2022-08-23 07:23:51 -0500

    Add Ball class

--- src/sprites/Ball.ts
+++ src/sprites/Ball.ts
@@ -1,3 +1,50 @@
 import { Vector2 } from "../types";
 
-export class Ball {}
+export class Ball {
+  private speed: Vector2;
+  private ballImage: HTMLImageElement = new Image();
+
+  constructor(
+    private ballSize: number,
+    private position: Vector2,
+    speed: number,
+    image: string
+  ) {
+    this.ballSize = ballSize;
+    this.position = position;
+    this.speed = {
+      x: speed,
+      y: -speed,
+    };
+    this.ballImage.src = image;
+  }
+
+  get width(): number {
+    return this.ballSize;
+  }
+
+  get height(): number {
+    return this.ballSize;
+  }
+
+  get pos(): Vector2 {
+    return this.position;
+  }
+
+  get image(): HTMLImageElement {
+    return this.ballImage;
+  }
+
+  changeYDir(): void {
+    this.speed.y = -this.speed.y;
+  }
+
+  changeXDir(): void {
+    this.speed.x = -this.speed.x;
+  }
+
+  moveBall(): void {
+    this.pos.x += this.speed.x;
+    this.pos.y += this.speed.y;
+  }
+}
